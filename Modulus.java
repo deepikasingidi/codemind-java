4439c10f733adbf61be3584f2ded1f64449ca576
deepikasingidi <147781264+deepikasingidi@users.noreply.github.com>
2024-08-01 23:59:14 +0530

    import java.util.Scanner; public class Modulus{     public static void main(String[] args){         int a,b;         Scanner scan = new Scanner(System.in);         a = scan.nextInt();         b = scan.nextInt();         System.out.println(a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Modulus{
+    public static void main(String[] args){
+        int a,b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a%b);
+    }
+}
